210f633c3473f1ecaf471e33abd5494ba6cafbd4
bryangwj <bryangwj@gmail.com>
2020-11-08 15:52:14 +0000

    Add relevant libraries, base codes for the main program and line following

---
+++ main_program/main_program.ino
@@ -0,0 +1,31 @@
+#include<Adafruit_MotorShield.h>
+
+#include<line_following.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *left_motor = AFMS.getMotor(1);
+Adafruit_DCMotor *right_motor = AFMS.getMotor(2);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  AFMS.begin();
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  long start = millis();
+  long end = start;
+
+  while((end - start) < 1000){
+    line_following();
+    end = millis();
+  }
+
+}
